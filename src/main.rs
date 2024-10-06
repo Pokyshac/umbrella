7dda5dcfa41bd505699310e2f57a25735a78c80a
Pokyshac <sabzirovi@mail.ru>
2024-10-06 16:09:11 +0400

    Init window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    
+#![allow(
+    dead_code,
+    unused_imports,
+    unused_variables
+)]
+
+use iced::Result;
+
+use umbrella::Umbrella;
+
+mod umbrella;
+mod ui;
+
+fn main() -> Result {
+    iced::application(Umbrella::title, 
+        Umbrella::update, 
+        Umbrella::view
+    ).run()
 }
--- src/umbrella.rs
+++ src/umbrella.rs
@@ -1,1 +1,28 @@
+use iced::{widget::{Column, Text}, Element, Task};
 
+#[derive(Debug, Default)]
+pub struct Umbrella {
+    
+}
+
+#[derive(Debug)]
+pub enum Message {
+    ASDA,
+}
+
+impl Umbrella {
+    pub fn title(&self) -> String {
+        String::from("Umbrella")
+    }
+
+    pub fn update(&mut self, message: Message) {
+    }
+
+    pub fn view(&self) -> Element<Message> {
+        Column::new().push(
+            Text::new("govno")
+        ).push(
+            Text::new("shit")
+        ).into()
+    }
+}
