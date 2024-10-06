c75b0968e7330f6b3d9508a05fdc5c31106f00d4
Pokyshac <sabzirovi@mail.ru>
2024-10-06 23:09:11 +0400

    Switch to slint gui('iced' - shit without docs(imho))

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,25 @@
     unused_variables
 )]
 
-use iced::Result;
+// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use umbrella::Umbrella;
+use std::error::Error;
 
-mod umbrella;
-mod ui;
+slint::include_modules!();
 
-fn main() -> Result {
-    iced::application(Umbrella::title, 
-        Umbrella::update, 
-        Umbrella::view
-    ).run()
+fn main() -> Result<(), Box<dyn Error>> {
+    let ui = AppWindow::new()?;
+
+    // ui.on_request_increase_value({
+    //     let ui_handle = ui.as_weak();
+    //     move || {
+    //         let ui = ui_handle.unwrap();
+    //         ui.set_counter(ui.get_counter() + 1);
+    //     }
+    // });
+
+    ui.run()?;
+
+    Ok(())
 }
